f730013594feac49dce7d552b147eaf4932006c8
Sasurarai <it242106@trident.ac.jp>
2026-02-03 11:23:33 +0900

    ゴールの基礎部分実装
    ゴールとそのためのスクリプトを作成

---
+++ Assets/Script/GoalGateContoroller.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/**
+ 	 * @file   GoalGateContoroller.cs
+ 	 *
+ 	 * @brief  ゴールゲートに関するソースファイル
+ 	 *
+ 	 * @author 制作者名　小塚太陽
+ 	 *
+ 	 * @date   最終更新日　2025/02/03
+ 	 */
+
+
+public class GoalGateContoroller : MonoBehaviour
+{
+    /*
+     * 
+     * @brief 初期化処理
+     * 
+     * @param[in] なし
+     * 
+     * @return なし
+     * 
+     */
+    void Start()
+    {
+        
+    }
+
+    /*
+     * @brief 更新処理
+     *
+     * @param[in] なし
+     * 
+     * @return なし
+     *
+     */
+    void Update()
+    {
+        
+    }
+
+    /*
+     * 
+     * @brief 横から当たった時の処理
+     * 
+     * @param[in] collision 当たったオブジェクト
+     * 
+     * @return なし
+     * 
+     */
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        
+    }
+}
