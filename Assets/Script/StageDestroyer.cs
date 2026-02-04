de54a387b1962a8335052bfcddcae087b70507b2
Sasurarai <it242106@trident.ac.jp>
2026-02-04 10:52:49 +0900

    ロゴ追加とデストロイヤー作成
    ごめんなさいロゴ追加しました。
    あとはステージ破壊用のスクリプトを進めました

--- Assets/Script/StageDestroyer.cs
+++ Assets/Script/StageDestroyer.cs
@@ -3,6 +3,22 @@ using System.Collections.Generic;
 using System.Threading;
 using UnityEngine;
 using UnityEngine.Tilemaps;
+using static UnityEditor.PlayerSettings;
+
+/*
+ *
+ * @brief ステージを徐々に崩していくクラス
+ * 
+ * @autor 制作者　小塚太陽
+ * 
+ * @detail ステージがどんどん崩れていく部分のクラスです。
+ * 
+ * 
+ */
+
+
+
+
 
 public class StageDestroyer : MonoBehaviour
 {
@@ -15,15 +31,42 @@ public class StageDestroyer : MonoBehaviour
 
     [SerializeField] int m_destroyCount = 1; ///< ステージを一気に破壊する列数
 
+    int m_currentDestroyRaw; ///<　今壊しているRaw;
 
-    // Start is called before the first frame update
+    int m_tileMapWidth; ///< タイルマップの横幅
+
+    int m_tileMapHeight; ///< タイルマップの縦幅
+
+
+    /*
+     *
+     * @brief 初期化処理
+     * 
+     * @param[in] なし
+     *
+     * @return なし
+     * 
+     */
     void Start()
     {
         //タイマーリセット
         m_timer = 0;
+
+        //タイルの幅から
+        m_tileMapWidth = m_tileMap.GetComponent<Tilemap>().cellBounds.x;
+        m_tileMapHeight = m_tileMap.GetComponent<Tilemap>().cellBounds.y;
     }
 
-    // Update is called once per frame
+  
+    /*
+     * 
+     * @brief 更新処理
+     * 
+     * @param[in] なし
+     * 
+     * @return なし
+     * 
+     */
     void Update()
     {
         //時間加算
@@ -33,10 +76,39 @@ public class StageDestroyer : MonoBehaviour
         //間隔が来たら破壊させる
         if(m_timer > m_destroyInterval)
         {
+            //設定した破壊数分破壊する
+            for (int i = 0; i < m_destroyCount; i++)
+            {
+                DestroyStage(i + m_currentDestroyRaw);
+            }
 
+            //破壊した行を加算（次に破壊する場所の指標にする）
+            m_currentDestroyRaw += m_destroyCount;
 
+            //タイマーリセット
             m_timer = 0;
         }
 
+    }
+
+    /*
+     * 
+     * @brief ステージの破壊処理
+     * 
+     * @param[in] x 破壊する行（X）
+     * 
+     * @return なし
+     * 
+     */
+    void DestroyStage(int x)
+    {
+        for(int y = 0;y < m_tileMapHeight;y++)
+        {
+            //x,yから位置を作る
+            Vector3Int pos = new(x, y,0);
+
+            //その位置のタイルをnullに置き換える
+            m_tileMap.SetTile(pos,null);
+        }
     }
 }
