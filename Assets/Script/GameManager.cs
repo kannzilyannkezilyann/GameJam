70a0f4016e65aa86cc7abedf94cb118927c17058
深谷翔太 <it242223@trident.ac.jp>
2026-02-04 13:17:54 +0900

    アイテムの実装UIの実装しました

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -59,8 +59,15 @@ public class GameManager : MonoBehaviour
      */
     void Update()
     {
-        
-    }
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+#if UNITY_EDITOR
+                UnityEditor.EditorApplication.isPlaying = false;//ゲームプレイ終了
+#else
+    Application.Quit();//ゲームプレイ終了
+#endif
+            }
+        }
 
     /**
     * @brief スコアの設定
--- Assets/Script/PlayerScript.cs
+++ Assets/Script/PlayerScript.cs
@@ -12,9 +12,17 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UIElements;
 
+// アイテムの列挙体の宣言 -------------------------------------------
+public enum ItemKinds
+{
+    NON,
+    TORCH,
+    OXYGEN_CYLINBER,
+    BALLOON
+}
 public class PlayerScript : MonoBehaviour 
 {
-// クラス定数の宣言 -------------------------------------------------
+    // クラス定数の宣言 -------------------------------------------------
     //プレイヤーの重さ
     public const float PLAYER_WIEGHT = 100.0f;
     
@@ -29,6 +37,8 @@ public class PlayerScript : MonoBehaviour
     [SerializeField] TreasureManager m_treasureManager;
     //false：タイトル用　true：ゲーム用
     [SerializeField] bool m_flag;
+    //プレイヤーが所持しているアイテム
+    private ItemKinds m_hasItem;
 
     [SerializeField] float      m_speed = 4.0f;
     [SerializeField] float      m_jumpForce = 7.5f;
@@ -39,16 +49,11 @@ public class PlayerScript : MonoBehaviour
     private Animator            m_animator;
     private Rigidbody2D         m_body2d;
     private Sensor_HeroKnight   m_groundSensor;
-    private Sensor_HeroKnight   m_wallSensorR1;
     private Sensor_HeroKnight   m_wallSensorR2;
-    private Sensor_HeroKnight   m_wallSensorL1;
     private Sensor_HeroKnight   m_wallSensorL2;
-    private bool                m_isWallSliding = false;
     private bool                m_grounded = false;
     private bool                m_rolling = false;
     private int                 m_facingDirection = 1;
-    private int                 m_currentAttack = 0;
-    private float               m_timeSinceAttack = 0.0f;
     private float               m_delayToIdle = 0.0f;
     private float               m_rollDuration = 8.0f / 14.0f;
     private float               m_rollCurrentTime;
@@ -59,20 +64,17 @@ public class PlayerScript : MonoBehaviour
         m_animator = GetComponent<Animator>();
         m_body2d = GetComponent<Rigidbody2D>();
         m_groundSensor = transform.Find("GroundSensor").GetComponent<Sensor_HeroKnight>();
-        m_wallSensorR1 = transform.Find("WallSensor_R1").GetComponent<Sensor_HeroKnight>();
         m_wallSensorR2 = transform.Find("WallSensor_R2").GetComponent<Sensor_HeroKnight>();
-        m_wallSensorL1 = transform.Find("WallSensor_L1").GetComponent<Sensor_HeroKnight>();
         m_wallSensorL2 = transform.Find("WallSensor_L2").GetComponent<Sensor_HeroKnight>();
         //リスト初期化
         m_takeTresures = new List<GameObject>();
+        //初期化
+        m_hasItem = ItemKinds.NON;
     }
 
     // Update is called once per frame
     void Update ()
     {
-        // Increase timer that controls attack combo
-        m_timeSinceAttack += Time.deltaTime;
-
         // Increase timer that checks roll duration
         if(m_rolling)
             m_rollCurrentTime += Time.deltaTime;
@@ -122,63 +124,8 @@ public class PlayerScript : MonoBehaviour
         //Set AirSpeed in animator
         m_animator.SetFloat("AirSpeedY", m_body2d.velocity.y);
 
-        // -- Handle Animations --
-        //Wall Slide
-        m_isWallSliding = (m_wallSensorR1.State() && m_wallSensorR2.State()) || (m_wallSensorL1.State() && m_wallSensorL2.State());
-        m_animator.SetBool("WallSlide", m_isWallSliding);
-
-        //Death
-        //if (Input.GetKeyDown("e") && !m_rolling)
-        //{
-        //    m_animator.SetBool("noBlood", m_noBlood);
-        //    m_animator.SetTrigger("Death");
-        //}
-            
-        //Hurt
-        //else if (Input.GetKeyDown("q") && !m_rolling)
-        //    m_animator.SetTrigger("Hurt");
-
-        //Attack
-        //if(Input.GetMouseButtonDown(0) && m_timeSinceAttack > 0.25f && !m_rolling && m_flag)
-        //{
-        //    m_currentAttack++;
-
-        //    // Loop back to one after third attack
-        //    if (m_currentAttack > 3)
-        //        m_currentAttack = 1;
-
-        //    // Reset Attack combo if time since last attack is too large
-        //    if (m_timeSinceAttack > 1.0f)
-        //        m_currentAttack = 1;
-
-        //    // Call one of three attack animations "Attack1", "Attack2", "Attack3"
-        //    m_animator.SetTrigger("Attack" + m_currentAttack);
-
-        //    // Reset timer
-        //    m_timeSinceAttack = 0.0f;
-        //}
-
-        // Block
-        //else if (Input.GetMouseButtonDown(1) && !m_rolling && m_flag)
-        //{
-        //    m_animator.SetTrigger("Block");
-        //    m_animator.SetBool("IdleBlock", true);
-        //}
-
-        //else if (Input.GetMouseButtonUp(1) && m_flag)
-        //    m_animator.SetBool("IdleBlock", false);
-
-        // Roll
-        if (Input.GetKeyDown("left shift") && !m_rolling && !m_isWallSliding && m_flag)
-        {
-            m_rolling = true;
-            m_animator.SetTrigger("Roll");
-            m_body2d.velocity = new Vector2(m_facingDirection * m_rollForce, m_body2d.velocity.y);
-        }
-            
-
         //Jump
-        else if (Input.GetKeyDown("space") && m_grounded && !m_rolling)
+        if (Input.GetKeyDown("space") && m_grounded && !m_rolling)
         {
             m_animator.SetTrigger("Jump");
             m_grounded = false;
@@ -326,6 +273,31 @@ public class PlayerScript : MonoBehaviour
     {
         return m_score;
     }
+
+    /**
+    * @brief アイテムの取得
+    *
+    * @param[in] なし
+    *
+    * @return 現在プレイヤーが所持しているアイテム
+    */
+    public ItemKinds GetItem()
+    {
+        return m_hasItem;
+    }
+
+    /**
+    * @brief アイテムのセット
+    *
+    * @param[in] なし
+    *
+    * @return なし
+    */
+    public void SetItem(ItemKinds item)
+    {
+        m_hasItem = item;
+    }
+
     /**
     * @brief 宝石を手に入れる
     *
