c21306d0913339f795ffb07eafb55715389be2e9
tktGamer <it242225@trident.ac.jp>
2026-02-05 15:27:25 +0900

    リザルトの不具合修正

--- Assets/Script/Treasure/BurstCoin.cs
+++ Assets/Script/Treasure/BurstCoin.cs
@@ -22,7 +22,7 @@ public class BurstCoin : MonoBehaviour
     //一コイン生成するためのスコア
     const int SCORE_TO_COIN_RATE = 30;
     //宝がコインに変わるまで
-    const float TREASURE_BURST = 0.8f;
+    const float TREASURE_BURST = 1.2f;
     // データメンバの宣言 -----------------------------------------------
     //コインプレハブ
     GameObject m_coinPrefab;
